02a9ffb9851c0e081bb30de85c38d2a21bece262
oldboydevelop <dev@oldboybarbershop.com>
2024-07-23 14:49:43 +0200

    cors

--- api/server.js
+++ api/server.js
@@ -3,32 +3,48 @@ import mongoose from "mongoose";
 import cors from "cors";
 import * as UserController from "./controllers/UserController.js";
 import dotenv from "dotenv";
-dotenv.config();
+
+dotenv.config(); // Загружаем переменные окружения
+
 const app = express();
+
+// Настройка CORS
 app.use(
   cors({
-    origin: "https://table-database.vercel.app/",
+    origin: "https://table-database.vercel.app", 
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
+
 app.use(express.json());
 
-const URL = "mongodb+srv://eolejnik:Eg_180587@cluster0.3xw5xo3.mongodb.net/";
-const port = 3000;
+// Использование переменных окружения
+const URL =
+  process.env.MONGO_URL ||
+  "mongodb+srv://eolejnik:Eg_180587@cluster0.3xw5xo3.mongodb.net/";
+const port = process.env.PORT || 3000;
+
+// Настройка маршрутов
 app.post("/users", UserController.add);
 app.get("/users", UserController.getAll);
 app.delete("/users/:id", UserController.remove);
 app.put("/users/:id", UserController.update);
 
+// Запуск сервера
 app.listen(port, (err) => {
-  err ? console.log(err) : console.log(`Сервер запущен на порту ${port}`);
+  if (err) {
+    console.error(`Error starting server: ${err}`);
+  } else {
+    console.log(`Server running on port ${port}`);
+  }
 });
 
+// Подключение к базе данных
 mongoose
   .connect(URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
   .then(() => console.log("Connected to MongoDB"))
-  .catch((err) => console.log(`DB connection error: ${err}`));
+  .catch((err) => console.error(`DB connection error: ${err}`));
